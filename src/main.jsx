eaba282d004ba9d1790848a6f621d72e7aef36b6
Vicky-clowdy <brianvickyx@gmail.com>
2025-06-22 17:48:28 +0530

    hash router

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,12 @@
 
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 
 createRoot(document.getElementById('root')).render(
 
-    <BrowserRouter basename='/Portfolio-25' >
+    <HashRouter >
     <App />
-    </BrowserRouter>
+    </HashRouter>
 
 )
